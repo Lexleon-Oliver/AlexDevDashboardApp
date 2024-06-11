acd335b9e1fee1be2e34936d019d088a916300a8
Lexleon-Oliver <lexleonmg@gmail.com>
2024-06-11 13:31:59 -0300

    ♻️ refactor: Padroniza formato de datas para DD/MM/AAAA HH:MM:SS

--- src/app/services/data-hora.service.ts
+++ src/app/services/data-hora.service.ts
@@ -18,7 +18,7 @@ export class DataHoraService {
     const minutos = String(dataAtual.getMinutes()).padStart(2, '0');
     const segundos = String(dataAtual.getSeconds()).padStart(2, '0');
 
-    const dataHoraFormatada = `${dia}-${mes}-${ano} ${horas}:${minutos}:${segundos}`;
+    const dataHoraFormatada = `${dia}/${mes}/${ano} ${horas}:${minutos}:${segundos}`;
 
     return dataHoraFormatada;
   }
